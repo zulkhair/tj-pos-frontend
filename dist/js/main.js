e656a9c02422322909d343eb5838dc32311d5f97
Zulkhair Abdullah Daim <zulkhair.daim@gmail.com>
2022-02-13 09:41:50 +0700

    initial commit

---
+++ dist/js/main.js
@@ -0,0 +1,1 @@
+$('#sidebarmenu').append('<li class="nav-item"><a href="#" class="nav-link">  <i class="nav-icon fas fa-th"></i>  <p>    Simple Link    <span class="right badge badge-danger">New</span>  </p></a></li>');
